1489d7dc8a9db5bedf8cff02adf64e08cc6c83f9
rahmonadeniji <rahmonadeniji317@gmail.com>
2023-08-21 22:52:23 +0100

    cached models

--- src/utils.py
+++ src/utils.py
@@ -15,18 +15,20 @@ import tensorflow as tf
 from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
 
-@st.cache
+
+
 class load_models:
 
     def __init__(self, model_path):
         self.model_path = model_path
 
-    @st.cache
+    
     def load_feature_extraction_model(self):
 
         image_feature_model = tf.keras.models.load_model(self.model_path)
 
         return image_feature_model
+
 
 obj = load_models("image_models/merged_model2.h5")
 m_model = obj.load_feature_extraction_model()
